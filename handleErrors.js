897d7c620dee0f28696b8f8eec2c85e2e63d83a7
Alex <alex.garcia.fdz@icloud.com>
2023-07-31 18:41:03 +0200

    Todo corregido y con fotos tmbn

--- handleErrors.js
+++ handleErrors.js
@@ -1,8 +1,12 @@
-module.exports = app.use((error, request, response, next) => {
-  console.error(error);
+module.exports = (error, request, response, next) => {
+  console.error(error)
   if (error.name === "CastError") {
-    response.status(400).send({ error: "id used is malformed" });
+    response.status(400).send({ error: "id used is malformed" })
   } else {
-    response.status(500).end();
+    response.status(500).end()
   }
-});
+}
+
+process.on("uncaughtException", () => {
+  mongoose.connection.disconnect()
+})
--- index.js
+++ index.js
@@ -1,94 +1,94 @@
-require("dotenv").config();
-require("./mongo.js");
+require("dotenv").config()
+require("./mongo.js")
 
-const Note = require("./models/Note.js");
+const Note = require("./models/Note.js")
 
-const express = require("express");
-const cors = require("cors");
-const logger = require("./loggerMiddleware");
-const notFound = require("./notFound.js");
-const handleErrors = require("./handleErrors.js");
+const express = require("express")
+const cors = require("cors")
+const logger = require("./loggerMiddleware")
+const notFound = require("./notFound.js")
+const handleErrors = require("./handleErrors.js")
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-
-app.use(logger);
+app.use(cors())
+app.use(express.json())
+app.use("/images", express.static("images"))
+app.use(logger)
 
 app.get("/", (request, response) => {
-  response.send("<h1>This is my API of notes!</h1>");
-});
+  response.send("<h1>This is my API of notes!</h1>")
+})
 
 app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
 app.get("/api/notes/:id", (request, response, next) => {
-  const { id } = request.params;
+  const { id } = request.params
   Note.findById(id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const { id } = request.params;
-  const note = request.body;
+  const { id } = request.params
+  const note = request.body
 
   const newNoteInfo = {
     content: note.content,
     important: note.important,
-  };
+  }
 
   Note.findByIdAndUpdate(id, newNoteInfo, { new: true }).then((result) => {
-    response.json(result);
-  });
-});
+    response.json(result)
+  })
+})
 
 app.delete("/api/notes/:id", (request, response, next) => {
-  const { id } = request.params;
+  const { id } = request.params
   Note.findByIdAndRemove(id)
     .then(() => {
-      response.status(204).end();
+      response.status(204).end()
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
+  const note = request.body
 
   if (!note.content) {
     return response
       .status(400)
-      .json({ error: 'required "content" field is missing' });
+      .json({ error: 'required "content" field is missing' })
   }
 
   const newNote = new Note({
     content: note.content,
     important: note.important || false,
     date: new Date(),
-  });
+  })
 
-  newNote.save().then((savedNote) => response.json(savedNote));
-});
+  newNote.save().then((savedNote) => response.json(savedNote))
+})
 
 //middlewares para errores
-app.use(notFound);
-app.use(handleErrors);
+app.use(notFound)
+app.use(handleErrors)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
